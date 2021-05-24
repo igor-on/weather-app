4506d910f2ceeb9f7503cd06b9f5b861935f225e
Igor Nowakowski <igornowakowski24@wp.pl>
2021-05-24 12:43:52 +0200

    Hibernate config

--- src/main/java/com/weather/weatherapp/WeatherApplication.java
+++ src/main/java/com/weather/weatherapp/WeatherApplication.java
@@ -1,7 +1,20 @@
 package com.weather.weatherapp;
 
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+
 public class WeatherApplication {
     public static void main(String[] args) {
+         StandardServiceRegistry registry = new StandardServiceRegistryBuilder()
+                .configure()
+                .build();
+
+         SessionFactory sessionFactory = new MetadataSources(registry)
+                .buildMetadata()
+                .buildSessionFactory();
+
         System.out.println("develop branch");
     }
 }
